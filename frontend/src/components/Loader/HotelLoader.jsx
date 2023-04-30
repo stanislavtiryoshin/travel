ff666c04eb8ae2953b10d5484e9d04d5587215e0
ald1k0n <cjube1337@gmail.com>
2023-04-30 21:40:32 +0600

    merged

--- frontend/src/components/Loader/HotelLoader.jsx
+++ frontend/src/components/Loader/HotelLoader.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 import styles from "./Loader.module.scss";
 
+import Loader from "../../components/Loader";
+
 const HotelLoader = () => {
-  return <div className={styles.hotel_loader}>Loading the hotel...</div>;
+  return (
+    <div className={styles.hotel_loader}>
+      <Loader />
+    </div>
+  );
 };
 
 export default HotelLoader;
